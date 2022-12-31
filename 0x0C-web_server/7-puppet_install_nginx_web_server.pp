d277a5ceb95c3d44421e35c38324ae64c396d2f0
Christian Koranteng <korantengchristian@gmail.com>
2022-12-31 17:47:37 +0000

    Update puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,22 +5,6 @@ package {'nginx':
 }
 
 exec {'install':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
   provider => shell,
-
-}
-
-exec {'Hello':
-  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  provider => shell,
-}
-
-exec {'sudo sed -i "s/server_name _;/server_name \_\;\n\tlocation \/redirect_me \{\n\t\treturn 301 \$scheme\:\/\/\$http_host\;\n\t\}\n/"
-/etc/nginx/sites-enabled/default':
-  provider => shell,
-}
-
-exec {'run':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/kkfergie22 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
